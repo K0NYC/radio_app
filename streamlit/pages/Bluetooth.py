c294656d67909048e710e20309989f49a11aa360
K0NYC <32525647+K0NYC@users.noreply.github.com>
2023-10-23 17:36:57 -0400

    added speaker

--- streamlit/pages/Bluetooth.py
+++ streamlit/pages/Bluetooth.py
@@ -1,21 +1,39 @@
 import streamlit as st
 import subprocess
 
-st.title('Bluetooth')
+st.set_page_config(
+    page_title='Bluetooth',
+    page_icon=':signal_strength:',
+)
 
-if st.button('Connect headphones'):
-    output = subprocess.run(['bluetoothctl', 'connect', '78:5E:A2:24:99:93'], capture_output=True, text=True).stdout
-    for line in output.split('\n'):
-        line
+with st.expander('AKG Headphones control', expanded=True):
+    if st.button('Info', key='headphones'):
+        output = subprocess.run(['bluetoothctl', 'info', '78:5E:A2:24:99:93'], capture_output=True, text=True).stdout
+        for line in output.split('\n'):
+            line
 
-if st.button('Disonnect headphones'):
-    output = subprocess.run(['bluetoothctl', 'disconnect', '78:5E:A2:24:99:93'], capture_output=True, text=True).stdout
-    for line in output.split('\n'):
-        line
+    if st.button('Connect headphones'):
+        output = subprocess.run(['bluetoothctl', 'connect', '78:5E:A2:24:99:93'], capture_output=True, text=True).stdout
+        for line in output.split('\n'):
+            line
 
-if st.button('Info'):
-    output = subprocess.run(['bluetoothctl', 'info', '78:5E:A2:24:99:93'], capture_output=True, text=True).stdout
-    for line in output.split('\n'):
-        line
+    if st.button('Disonnect headphones'):
+        output = subprocess.run(['bluetoothctl', 'disconnect', '78:5E:A2:24:99:93'], capture_output=True, text=True).stdout
+        for line in output.split('\n'):
+            line
 
+with st.expander("JBL speaker control", expanded=True):
+    if st.button('Info', key='speaker'):
+        output = subprocess.run(['bluetoothctl', 'info', '5C:FB:7C:BF:E0:97'], capture_output=True, text=True).stdout
+        for line in output.split('\n'):
+            line
 
+    if st.button('Connect speaker'):
+        output = subprocess.run(['bluetoothctl', 'connect', '5C:FB:7C:BF:E0:97'], capture_output=True, text=True).stdout
+        for line in output.split('\n'):
+            line
+
+    if st.button('Disonnect speaker'):
+        output = subprocess.run(['bluetoothctl', 'disconnect', '5C:FB:7C:BF:E0:97'], capture_output=True, text=True).stdout
+        for line in output.split('\n'):
+            line
